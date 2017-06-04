eff99fcd861aabc2b7f6aebd6a569777375f8234
zongzi531 <zongzi.xy@gmail.com>
2017-06-04 15:29:56 +0800

    update readme
    那个“不必要的console.log”其实很有用……

--- src/App.vue
+++ src/App.vue
@@ -72,9 +72,12 @@ export default {
     }
   },
   mounted () {
-    this.$dragging.$on('dragged', () => {
+    this.$dragging.$on('dragged', ({value}) => {
       this.changeflag = -1
       this.changeText = ''
+      console.log(value.item)
+      console.log(value.list)
+      console.log(value.otherData)
     })
   },
   methods: {
